6911d34f3fda700015fbbcd4b9c65023261d23ad
Florian Wilhelm <florian.wilhelm02@sap.com>
2024-04-29 13:41:10 +0200

    init

---
+++ src/main/java/io/gardenlinux/glvd/GlvdApplication.java
@@ -0,0 +1,13 @@
+package io.gardenlinux.glvd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GlvdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GlvdApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/gardenlinux/glvd/GlvdApplicationTests.java
@@ -0,0 +1,13 @@
+package io.gardenlinux.glvd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GlvdApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
