257f4d60b6d6069cef7ec9c9d908fe764ed5a3f1
Florian Wilhelm <2292245+fwilhe@users.noreply.github.com>
2024-05-03 11:17:49 +0200

    Setup tests in testcontainers (#2)

--- src/test/java/io/gardenlinux/glvd/GlvdApplicationTests.java
+++ src/test/java/io/gardenlinux/glvd/GlvdApplicationTests.java
@@ -2,9 +2,23 @@ package io.gardenlinux.glvd;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
+import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+import org.testcontainers.utility.DockerImageName;
 
-@SpringBootTest
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@Testcontainers
 class GlvdApplicationTests {
+    static DockerImageName glvdPostgresImage = DockerImageName.parse("ghcr.io/gardenlinux/glvd-postgres:edgesampledata")
+            .asCompatibleSubstituteFor("postgres");
+
+    @Container
+    @ServiceConnection
+    static PostgreSQLContainer<?> postgres = new PostgreSQLContainer<>(
+            glvdPostgresImage
+    ).withDatabaseName("glvd").withUsername("glvd").withPassword("glvd");
 
     @Test
     void contextLoads() {
