86f06159653b0336212a74e8119bfac6448fc462
Florian Wilhelm <florian.wilhelm02@sap.com>
2024-05-10 14:49:57 +0200

    Add script to run tests on mac with podman

---
+++ test-on-mac-with-podman.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+# As documented here https://java.testcontainers.org/supported_docker_environment/#podman
+
+ export DOCKER_HOST=unix://$(podman machine inspect --format '{{.ConnectionInfo.PodmanSocket.Path}}')
+ export TESTCONTAINERS_DOCKER_SOCKET_OVERRIDE=/var/run/docker.sock
+ export TESTCONTAINERS_RYUK_DISABLED=true
+
+ ./gradlew --info test
