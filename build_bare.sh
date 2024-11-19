37cb7ad351d57f4e381c5b313df6b78476e70e5f
Florian Wilhelm <florian.wilhelm02@sap.com>
2024-11-19 14:21:25 +0100

    wip

--- build_bare.sh
+++ build_bare.sh
@@ -1,21 +1,20 @@
 #!/bin/bash
 
+set -x
+
 GLVD_API_IMAGE_REPOSITORY=ghcr.io/gardenlinux/glvd-api
 GLVD_API_IMAGE_TAG=latest
 
 build () {
     local ARCH="${1}"; shift
 
-    SHA_GLVD=$(podman pull -q --arch="$ARCH" $GLVD_API_IMAGE_REPOSITORY:$GLVD_API_IMAGE_TAG)
-    podman save --format oci-archive "$SHA_GLVD" > glvd-"$ARCH".oci
+    # SHA_GLVD=$(podman pull -q --arch="$ARCH" $GLVD_API_IMAGE_REPOSITORY:$GLVD_API_IMAGE_TAG)
+    # SHA_GL=$(podman pull -q --arch="$ARCH" ghcr.io/gardenlinux/gardenlinux:1592)
 
-    SHA_GL=$(podman pull -q --arch="$ARCH" ghcr.io/gardenlinux/gardenlinux:1592)
-    podman save --format oci-archive "$SHA_GL" > gardenlinux-"$ARCH".oci
+    # ./unbase_oci --arch "$ARCH" --exclude exclude --include include --ldd-dependencies --print-tree podman:ghcr.io/gardenlinux/gardenlinux:1592@sha256:$SHA_GL podman:ghcr.io/gardenlinux/glvd-api:latest@sha256:$SHA_GLVD :latest-linux${ARCH}_bare
 
-    ./unbase_oci --exclude exclude --include include --ldd-dependencies --print-tree gardenlinux-"$ARCH".oci glvd-"$ARCH".oci glvd_bare-"$ARCH".oci
+    ./unbase_oci --arch "$ARCH" --exclude exclude --include include --ldd-dependencies --print-tree podman:ghcr.io/gardenlinux/gardenlinux:1592 podman:ghcr.io/gardenlinux/glvd-api:latest :latest-linux${ARCH}_bare
 
-    image="$(podman load < glvd_bare-"$ARCH".oci | awk '{ print $NF }')"
-    podman tag "$image" $GLVD_API_IMAGE_REPOSITORY:$GLVD_API_IMAGE_TAG-linux"$ARCH"_bare
 }
 
 build amd64
