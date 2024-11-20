c0029fda47761e5571020275a23e4e3334f98219
Florian Wilhelm <2292245+fwilhe@users.noreply.github.com>
2024-11-20 11:00:31 +0100

    Use new unbase_oci script (#57)
    Simplify build pipeline by making use of the new features in unbase_oci as described in gardenlinux/gardenlinux#2476

--- build_bare.sh
+++ build_bare.sh
@@ -7,15 +7,10 @@ build () {
     local ARCH="${1}"; shift
 
     SHA_GLVD=$(podman pull -q --arch="$ARCH" $GLVD_API_IMAGE_REPOSITORY:$GLVD_API_IMAGE_TAG)
-    podman save --format oci-archive "$SHA_GLVD" > glvd-"$ARCH".oci
-
     SHA_GL=$(podman pull -q --arch="$ARCH" ghcr.io/gardenlinux/gardenlinux:1592)
-    podman save --format oci-archive "$SHA_GL" > gardenlinux-"$ARCH".oci
 
-    ./unbase_oci --exclude exclude --include include --ldd-dependencies --print-tree gardenlinux-"$ARCH".oci glvd-"$ARCH".oci glvd_bare-"$ARCH".oci
+    ./unbase_oci --exclude exclude --include include --ldd-dependencies --print-tree podman:"$SHA_GL" podman:"$SHA_GLVD" podman:ghcr.io/gardenlinux/glvd-api:latest-linux${ARCH}_bare
 
-    image="$(podman load < glvd_bare-"$ARCH".oci | awk '{ print $NF }')"
-    podman tag "$image" $GLVD_API_IMAGE_REPOSITORY:$GLVD_API_IMAGE_TAG-linux"$ARCH"_bare
 }
 
 build amd64
