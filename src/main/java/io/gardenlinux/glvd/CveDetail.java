cba928e03e7ba1357311a8b9a1ffb39d9903d29b
Florian Wilhelm <florian.wilhelm02@sap.com>
2025-04-22 13:06:26 +0200

    refactor

--- src/main/java/io/gardenlinux/glvd/CveDetail.java
+++ src/main/java/io/gardenlinux/glvd/CveDetail.java
@@ -8,7 +8,8 @@ import java.util.List;
 
 public record CveDetail(String cveId, String vulnStatus, String description, String cvePublishedDate,
                         String cveModifiedDate, String cveIngestedDate, List<String> kernelLtsVersion,
-                        List<String> kernelFixedVersion, List<Boolean> kernelIsFixed, List<Boolean> kernelIsRelevantSubsystem,
+                        List<String> kernelFixedVersion, List<Boolean> kernelIsFixed,
+                        List<Boolean> kernelIsRelevantSubsystem,
                         List<String> distro, List<String> distroVersion, List<Boolean> isVulnerable,
                         List<String> sourcePackageName, List<String> sourcePackageVersion, List<String> versionFixed,
                         Float baseScoreV40, Float baseScoreV31, Float baseScoreV30, Float baseScoreV2,
@@ -16,67 +17,56 @@ public record CveDetail(String cveId, String vulnStatus, String description, Str
 
 
     static Boolean compareVersions(String mine, String yours) {
-
-        var foo = LinuxKernelVersion.fromRawVersion(mine);
-        var bar = LinuxKernelVersion.fromRawVersion(yours);
-        if (foo.compareTo(bar) < 0) {
+        var myKernelVersion = LinuxKernelVersion.fromRawVersion(mine);
+        var yourKernelVersion = LinuxKernelVersion.fromRawVersion(yours);
+        if (myKernelVersion.compareTo(yourKernelVersion) < 0) {
             return Boolean.TRUE;
         }
 
         return Boolean.FALSE;
     }
 
-
-    static List<Boolean> determineGlVulnerability(KernelCveDetails k, KernelDistroVersions kernelDistroVersions) {
-        var ret = new ArrayList<Boolean>(kernelDistroVersions.distros().size());
-        for (int i = 0; i < kernelDistroVersions.distros().size(); i++) {
-            if (kernelDistroVersions.distros().get(i).equals("gardenlinux")) {
-                var myKernelVersion = kernelDistroVersions.kernelVersions().get(i);
-                for (String lts : k.getLtsVersion()) {
-                    if (myKernelVersion.startsWith(lts)) {
-                        var yourKernelVersions = k.getFixedVersion().stream().filter(s -> s != null && s.startsWith(lts)).toList();
-                        if (yourKernelVersions.size() == 1) {
-                            ret.add(compareVersions(myKernelVersion, yourKernelVersions.getFirst()));
-                        } else {
-                            ret.add(Boolean.TRUE);
-                        }
-                    }
+    static boolean isKernelVersionVulnerable(KernelCveDetails k, String myKernelVersion) {
+        for (String lts : k.getLtsVersion()) {
+            if (myKernelVersion.startsWith(lts)) {
+                var yourKernelVersions = k.getFixedVersion().stream().filter(s -> s != null && s.startsWith(lts)).toList();
+                if (yourKernelVersions.size() == 1) {
+                    return compareVersions(myKernelVersion, yourKernelVersions.getFirst());
+                } else {
+                    return true;
                 }
             }
-
         }
-        return ret;
+        return true;
     }
 
     static CveDetail fromKernelCve(KernelCveDetails k, KernelDistroVersions kernelDistroVersions) {
-        var glVulnerability = determineGlVulnerability(k, kernelDistroVersions);
-        DistroInfo result = filterNonGardenLinuxDistros(kernelDistroVersions);
+        KernelDistroInfo d = buildKernelInfoForGardenLinuxDistros(k, kernelDistroVersions);
         return new CveDetail(k.getCveId(), k.getVulnStatus(), k.getDescription(), k.getCvePublishedDate(),
                 k.getCveModifiedDate(), k.getCveIngestedDate(), k.getLtsVersion(),
-                k.getFixedVersion(), k.getIsFixed(), k.getIsRelevantSubsystem(), result.distros(), result.distroVersions(),
-                glVulnerability, result.sourcePackageNames(), result.kernelVersions(),
+                k.getFixedVersion(), k.getIsFixed(), k.getIsRelevantSubsystem(), d.distros(), d.distroVersions(),
+                d.isVulnerable(), d.sourcePackageNames(), d.kernelVersions(),
                 k.getFixedVersion(), k.getBaseScoreV40(), k.getBaseScoreV31(), k.getBaseScoreV30(), k.getBaseScoreV2(),
                 k.getVectorStringV40(), k.getVectorStringV31(),
                 k.getVectorStringV30(), k.getVectorStringV2());
     }
 
-    private static DistroInfo filterNonGardenLinuxDistros(KernelDistroVersions kernelDistroVersions) {
+    private static KernelDistroInfo buildKernelInfoForGardenLinuxDistros(KernelCveDetails k, KernelDistroVersions kernelDistroVersions) {
         var distros = new ArrayList<String>(kernelDistroVersions.distros().size());
         var distroVersions = new ArrayList<String>(kernelDistroVersions.distros().size());
         var sourcePackageNames = new ArrayList<String>(kernelDistroVersions.distros().size());
         var kernelVersions = new ArrayList<String>(kernelDistroVersions.distros().size());
+        var vulnerability = new ArrayList<Boolean>(kernelDistroVersions.distros().size());
         for (int i = 0; i < kernelDistroVersions.distros().size(); i++) {
             if (kernelDistroVersions.distros().get(i).equals("gardenlinux")) {
                 distros.add(kernelDistroVersions.distros().get(i));
                 distroVersions.add(kernelDistroVersions.versions().get(i));
-                sourcePackageNames.add("linux");
+                sourcePackageNames.add(kernelDistroVersions.sourcePackageName().get(i));
                 kernelVersions.add(kernelDistroVersions.kernelVersions().get(i));
+                vulnerability.add(isKernelVersionVulnerable(k, kernelDistroVersions.kernelVersions().get(i)));
             }
         }
-        return new DistroInfo(distros, distroVersions, sourcePackageNames, kernelVersions);
-    }
-
-    private record DistroInfo(ArrayList<String> distros, ArrayList<String> distroVersions, ArrayList<String> sourcePackageNames, ArrayList<String> kernelVersions) {
+        return new KernelDistroInfo(distros, distroVersions, sourcePackageNames, kernelVersions, vulnerability);
     }
 
     static CveDetail fromDebianCve(CveDetails c) {
@@ -87,5 +77,10 @@ public record CveDetail(String cveId, String vulnStatus, String description, Str
                 c.getVersionFixed(), c.getBaseScoreV40(), c.getBaseScoreV31(), c.getBaseScoreV30(),
                 c.getBaseScoreV2(), c.getVectorStringV40(), c.getVectorStringV31(),
                 c.getVectorStringV30(), c.getVectorStringV2());
+    }
+
+    private record KernelDistroInfo(ArrayList<String> distros, ArrayList<String> distroVersions,
+                                    ArrayList<String> sourcePackageNames, ArrayList<String> kernelVersions,
+                                    ArrayList<Boolean> isVulnerable) {
     }
 }
