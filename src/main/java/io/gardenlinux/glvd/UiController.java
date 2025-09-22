b8ae1395747dbb17b3662a774a091594b09e88b8
Florian Wilhelm <florian.wilhelm02@sap.com>
2025-09-22 16:12:49 +0200

    fix

--- src/main/java/io/gardenlinux/glvd/UiController.java
+++ src/main/java/io/gardenlinux/glvd/UiController.java
@@ -5,7 +5,6 @@ import io.gardenlinux.glvd.exceptions.CveNotKnownException;
 import jakarta.annotation.Nonnull;
 import org.commonmark.parser.Parser;
 import org.commonmark.renderer.html.HtmlRenderer;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
--- src/main/java/io/gardenlinux/glvd/db/NvdCveDataAttributeConverter.java
+++ src/main/java/io/gardenlinux/glvd/db/NvdCveDataAttributeConverter.java
@@ -2,22 +2,20 @@ package io.gardenlinux.glvd.db;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import io.gardenlinux.glvd.GlvdService;
 import jakarta.persistence.AttributeConverter;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class NvdCveDataAttributeConverter implements AttributeConverter<NvdCve.Data, String> {
     private static final ObjectMapper objectMapper = new ObjectMapper();
-    Logger logger = LoggerFactory.getLogger(GlvdService.class);
-
+    Logger logger = LoggerFactory.getLogger(NvdCveDataAttributeConverter.class);
 
     @Override
     public String convertToDatabaseColumn(NvdCve.Data attribute) {
         try {
             return objectMapper.writeValueAsString(attribute);
         } catch (JsonProcessingException jpe) {
-            logger.warn("Cannot convert Address into JSON");
+            logger.warn("Cannot convert CVE Data into JSON");
             return null;
         }
     }
@@ -27,7 +25,7 @@ public class NvdCveDataAttributeConverter implements AttributeConverter<NvdCve.D
         try {
             return objectMapper.readValue(dbData, NvdCve.Data.class);
         } catch (JsonProcessingException e) {
-            logger.warn("Cannot convert JSON into Address");
+            logger.warn("Cannot convert JSON into CVE Data");
             return null;
         }
     }
